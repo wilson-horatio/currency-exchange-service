364f9de4f3487a955367cd7cef331f1ab9301394
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2024-01-21 17:56:17 +0100

    OpenTelemetry and ZipKin

--- src/main/java/com/horatiowilson/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/horatiowilson/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.horatiowilson.currencyexchangeservice.controller;
 import com.horatiowilson.currencyexchangeservice.dto.CurrencyExchangeDto;
 import com.horatiowilson.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import com.horatiowilson.currencyexchangeservice.repository.CurrencyExchangeRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
@@ -22,6 +26,7 @@ public class CurrencyExchangeController {
     public CurrencyExchangeDto retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchangeRepositoryImpl currencyExchangeRepositoryImpl = currencyExchangeRepository.findByFromAndTo(from, to);
         if (currencyExchangeRepositoryImpl == null) {
             throw new RuntimeException("Nothing found in db");
